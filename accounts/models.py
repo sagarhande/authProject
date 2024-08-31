815b58a1f3690b43a94961512e4190ee26a153d6
sagarhande <sagarhande1947@gmail.com>
2024-08-31 12:54:16 +0530

    minor changes in model

--- accounts/models.py
+++ accounts/models.py
@@ -50,7 +50,6 @@ class Account(AbstractBaseUser):
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username", "first_name", "last_name"]
@@ -62,9 +61,3 @@ class Account(AbstractBaseUser):
 
     def __str__(self):
         return self.email
-
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
-
-    def has_module_perms(self, add_label):
-        return True
